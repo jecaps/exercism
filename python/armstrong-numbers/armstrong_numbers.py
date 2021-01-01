1ef21faffb097cc2608b5168bf9392d0abd74a01
Jerika Capila <jerikacapila@gmail.com>
2021-01-01 19:11:06 +0100

    Complete Armstrong Numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong_number(number):
-    pass
+    total = 0
+    for x in str(number):
+        total += int(x) ** len(str(number))
+    if total == number:
+        return True
+    else:
+        return False
