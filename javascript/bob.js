a6b7e844f4d090599a0ca1a866dc0605caaf1a27
Jerika Kanz <jerikacapila@gmail.com>
2022-08-18 08:27:14 +0200

    Edit Bob

--- javascript/bob.js
+++ javascript/bob.js
@@ -17,15 +17,25 @@ Bob's conversational partner is a purist when it comes to written communication 
 
 const hey = (message) => {
   message = message.trim();
-  if (/^[\s]*$/.test(message)) {
-    return "Fine. Be that way!";
-  } else if (/^[A-Z][A-Z\W]+\?$/.test(message)) {
-    return "Calm down, I know what I'm doing!";
-  } else if (/^[\w\W]+\?$/.test(message)) {
-    return "Sure.";
-  } else if (/^(?=.*[A-Z])[A-Z0-9\W]*$/.test(message)) {
-    return "Whoa, chill out!";
-  } else {
-    return "Whatever.";
-  }
+  // if (/^[\s]*$/.test(message)) {
+  //   return "Fine. Be that way!";
+  // } else if (/^[A-Z][A-Z\W]+\?$/.test(message)) {
+  //   return "Calm down, I know what I'm doing!";
+  // } else if (/^[\w\W]+\?$/.test(message)) {
+  //   return "Sure.";
+  // } else if (/^(?=.*[A-Z])[A-Z0-9\W]*$/.test(message)) {
+  //   return "Whoa, chill out!";
+  // } else {
+  //   return "Whatever.";
+  // }
+
+  return /^[\s]*$/.test(message)
+    ? "Fine. Be that way!"
+    : /^[A-Z][A-Z\W]+\?$/.test(message)
+    ? "Calm down, I know what I'm doing!"
+    : /^[\w\W]+\?$/.test(message)
+    ? "Sure."
+    : /^(?=.*[A-Z])[A-Z0-9\W]*$/.test(message)
+    ? "Whoa, chill out!"
+    : "Whatever.";
 };
