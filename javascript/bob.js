d5e13122415ed15a138f8d9361bb509e881d8dcb
Jerika Kanz <jerikacapila@gmail.com>
2022-08-18 08:00:48 +0200

    Bob

---
+++ javascript/bob.js
@@ -0,0 +1,31 @@
+/*
+Instructions
+Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question, such as "How are you?".
+
+He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+
+Bob's conversational partner is a purist when it comes to written communication and always follows normal rules regarding sentence punctuation in English.
+*/
+
+const hey = (message) => {
+  message = message.trim();
+  if (/^[\s]*$/.test(message)) {
+    return "Fine. Be that way!";
+  } else if (/^[A-Z][A-Z\W]+\?$/.test(message)) {
+    return "Calm down, I know what I'm doing!";
+  } else if (/^[\w\W]+\?$/.test(message)) {
+    return "Sure.";
+  } else if (/^(?=.*[A-Z])[A-Z0-9\W]*$/.test(message)) {
+    return "Whoa, chill out!";
+  } else {
+    return "Whatever.";
+  }
+};
